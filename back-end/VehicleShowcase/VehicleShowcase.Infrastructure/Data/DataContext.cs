1e50befe2a9be1ff440cd5f9a770e15f521622be
Matheus Henrique <matheushenr941@gmail.com>
2023-07-17 23:14:58 -0300

    add data context

---
+++ back-end/VehicleShowcase/VehicleShowcase.Infrastructure/Data/DataContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace VehicleShowcase.Infrastructure.Data
+{
+    public class DataContext: DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+    }
+}
