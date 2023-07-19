0a6da9178e96be69954e023b491f7d7caa002f1a
Matheus Henrique <matheushenr941@gmail.com>
2023-07-18 21:33:17 -0300

    add dbset admin/vehicle

--- back-end/VehicleShowcase/VehicleShowcase.Infrastructure/Data/DataContext.cs
+++ back-end/VehicleShowcase/VehicleShowcase.Infrastructure/Data/DataContext.cs
@@ -1,9 +1,13 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using VehicleShowcase.Domain.Entities;
 
 namespace VehicleShowcase.Infrastructure.Data
 {
     public class DataContext: DbContext
     {
         public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<Vehicle> Vehicles => Set<Vehicle>();
+        public DbSet<Admin> Admins => Set<Admin>();
     }
 }
