152e4d2bf83944b7e345861e690038481811c888
Matheus Henrique <matheushenr941@gmail.com>
2023-07-18 22:35:55 -0300

    add admin/vehicle controllers

---
+++ back-end/VehicleShowcase/VehicleShowcase.Web/Controllers/AdminController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using VehicleShowcase.Application.DTOs.Admin;
+using VehicleShowcase.Application.DTOs.AdminUser;
+using VehicleShowcase.Application.Interfaces;
+
+namespace VehicleShowcase.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AdminController : ControllerBase
+    {
+        private readonly IAdminService _adminService;
+
+        public AdminController(IAdminService adminService)
+        {
+            _adminService = adminService;                
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<GetAdminResponseDTO>> GetAdminByIdAsync(int id)
+        {
+            var admin = await _adminService.GetAdminByIdAsync(id);
+
+            if (admin is null)
+                return NotFound(admin);
+
+            return Ok(admin);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<GetAdminResponseDTO>> AddAdminAsync(AddAdminRequestDTO newAdmin)
+        {
+            var admin = await _adminService.AddAdminAsync(newAdmin);
+
+            return Ok(admin);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteAdminAsync(int id)
+        {
+            await _adminService.DeleteAdminAsync(id);
+
+            return Ok();
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<GetAdminResponseDTO>> UpdateAdminAsync(int id, EditAdminRequestDTO newAdmin)
+        {
+            var admin = await _adminService.UpdateAdminAsync(id, newAdmin);
+
+            if (admin is null)
+                return NotFound(admin);
+
+            return Ok(admin);
+        }
+    }
+}
---
+++ back-end/VehicleShowcase/VehicleShowcase.Web/Controllers/VehicleController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using VehicleShowcase.Application.DTOs.Admin;
+using VehicleShowcase.Application.DTOs.Vehicle;
+using VehicleShowcase.Application.Interfaces;
+
+namespace VehicleShowcase.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VehicleController : ControllerBase
+    {
+        private readonly IVehicleService _vehicleService;
+
+        public VehicleController(IVehicleService vehicleService)
+        {
+            _vehicleService = vehicleService;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<GetVehicleResponseDTO>> GetVehicleByIdAsync(int id)
+        {
+            var vehicle = await _vehicleService.GetVehicleByIdAsync(id);
+
+            if (vehicle is null)
+                return NotFound(vehicle);
+
+            return Ok(vehicle);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<GetAdminResponseDTO>> AddVehicleAsync(AddVehicleRequestDTO newVehicle)
+        {
+            var vehicle = await _vehicleService.AddVehicleAsync(newVehicle);
+            return Ok(vehicle);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteVehicleAsync(int id)
+        {
+            await _vehicleService.DeleteVehicleAsync(id);
+
+            return Ok();
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<GetAdminResponseDTO>> UpdateVehicleAsync(int id, AddVehicleRequestDTO newVehicle)
+        {
+            var vehicle = await _vehicleService.UpdateVehicleAsync(id, newVehicle);
+
+            if (vehicle is null)
+                return NotFound(vehicle);
+
+            return Ok(vehicle);
+        }
+    }
+}
