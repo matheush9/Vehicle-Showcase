cfce966de1de532e5daf2386c6427d82aab9bfa0
Matheus Henrique <matheushenr941@gmail.com>
2023-07-18 21:54:36 -0300

    add admin/vehicle mapper profiles

---
+++ back-end/VehicleShowcase/VehicleShowcase.Application/Mappers/AdminMapperProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using VehicleShowcase.Application.DTOs.Admin;
+using VehicleShowcase.Application.DTOs.AdminUser;
+using VehicleShowcase.Domain.Entities;
+
+namespace VehicleShowcase.Application.Mappers
+{
+    public class AdminMapperProfile: Profile
+    {
+        public AdminMapperProfile()
+        {
+            CreateMap<Admin, GetAdminResponseDTO>();
+            CreateMap<Admin, AddAdminRequestDTO>();
+            CreateMap<AddAdminRequestDTO, Admin>();
+        }
+    }
+}
---
+++ back-end/VehicleShowcase/VehicleShowcase.Application/Mappers/VehicleMapperProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using VehicleShowcase.Application.DTOs.Vehicle;
+using VehicleShowcase.Domain.Entities;
+
+namespace VehicleShowcase.Application.Mappers
+{
+    public class VehicleMapperProfile : Profile
+    {
+        public VehicleMapperProfile()
+        {
+            CreateMap<Vehicle, GetVehicleResponseDTO>();
+            CreateMap<Vehicle, AddVehicleRequestDTO>();
+            CreateMap<AddVehicleRequestDTO, Vehicle>();
+        }
+    }
+}
