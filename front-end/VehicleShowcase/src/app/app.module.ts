1873211437fdccdd1ec3d8a8bee37a95eee6634b
Matheus Henrique <matheushenr941@gmail.com>
2023-07-21 23:26:15 -0300

    build add material

--- front-end/VehicleShowcase/src/app/app.module.ts
+++ front-end/VehicleShowcase/src/app/app.module.ts
@@ -5,16 +5,18 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './modules/core/core.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    CoreModule
+    CoreModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
