892843b1b8c1fdec2c4272a20c99627287b27354
Matheus Henrique <matheushenr941@gmail.com>
2023-07-22 22:01:37 -0300

    update prod environment

--- front-end/VehicleShowcase/src/environments/environment.ts
+++ front-end/VehicleShowcase/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    apiUrl: 'http://154.41.228.116:81/api',
-    imagesProviderUrl: 'http://154.41.228.116:81'
+    apiUrl: 'http://154.41.228.116:8002/api',
+    imagesProviderUrl: 'http://154.41.228.116:8002'
 };
